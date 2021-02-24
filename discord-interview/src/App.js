427d6f22828bd78eae3732013dea8ec002558ab4
Jim Segal <hello@jimsegal.com>
2021-02-23 21:55:00 -0600

    complete the challenge

--- discord-interview/src/App.js
+++ discord-interview/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "react-modal";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [serverName, setServerName] = useState("");
+  const [serverList, setServerList] = useState({});
+  const [selectedServer, setSelectedServer] = useState("");
+  const [chatVal, setChat] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setModalOpen(true)}>open the modal!</button>
+      <Modal
+        isOpen={isModalOpen}
+        style={customStyles}
+        onRequestClose={() => setModalOpen(false)}
+        contentLabel="Discord Modal"
+      >
+        <h2>Server</h2>
+        <button onClick={() => setModalOpen(false)}>close</button>
+        <div className="container">
+          <div>
+            <h3>Add a server</h3>
+            <div className="server">
+              <label>
+                Add a new Server
+                <input
+                  type="text"
+                  value={serverName}
+                  onChange={(e) => setServerName(e.target.value)}
+                />
+                <button
+                  onClick={() =>
+                    setServerList({
+                      ...serverList,
+                      [serverName]: {
+                        chat: [],
+                      },
+                    })
+                  }
+                >
+                  Add new server
+                </button>
+              </label>
+              {!!Object.keys(serverList).length ? (
+                <ul>
+                  {Object.keys(serverList).map((server) => {
+                    return (
+                      <li
+                        style={{
+                          border: serverList[server].selected
+                            ? "2px solid green"
+                            : "none",
+                        }}
+                        onClick={() => setSelectedServer(server)}
+                      >
+                        {server}
+                      </li>
+                    );
+                  })}
+                </ul>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <h3>Chat</h3>
+            {!!selectedServer ? (
+              <>
+                {serverList[selectedServer].chat.map((c) => (
+                  <p>{c}</p>
+                ))}
+                <input
+                  type="text"
+                  placeholder="type here"
+                  value={chatVal}
+                  onChange={(e) => setChat(e.target.value)}
+                />
+                <button
+                  onClick={() => {
+                    setServerList({
+                      ...serverList,
+                      [selectedServer]: {
+                        ...serverList[selectedServer],
+                        chat: [...serverList[selectedServer].chat, chatVal],
+                      },
+                    });
+                  }}
+                >
+                  send
+                </button>
+              </>
+            ) : (
+              <div>Click a server from the list on the left to chat</div>
+            )}
+          </div>
+        </div>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
