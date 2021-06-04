b8cd551c193c5662a604abae6336e8bc63693f74
Jim Segal <hello@jimsegal.com>
2021-06-03 22:59:36 -0500

    exchange guard function for another terrible level of nested ifs

--- ransom/lib/ransom.ex
+++ ransom/lib/ransom.ex
@@ -3,21 +3,21 @@ defmodule Ransom do
   Documentation for `Ransom`.
   """
   @spec can_scramble?(String.t(), String.t()) :: boolean()
-  def can_scramble?(available_letters, message)
-      when length(available_letters) < length(message),
-      do: false
-
   def can_scramble?(available_letters, message) do
-    al_char_count_map = to_char_count_map(available_letters)
-    message_char_count_map = to_char_count_map(message)
+    if String.length(available_letters) >= String.length(message) do
+      al_char_count_map = to_char_count_map(available_letters)
+      message_char_count_map = to_char_count_map(message)
 
-    Enum.reduce(message_char_count_map, true, fn {message_char, message_char_count}, acc ->
-      if is_nil(al_char_count_map[message_char]) do
-        false
-      else
-        if acc && al_char_count_map[message_char] >= message_char_count, do: true, else: false
-      end
-    end)
+      Enum.reduce(message_char_count_map, true, fn {message_char, message_char_count}, acc ->
+        if is_nil(al_char_count_map[message_char]) do
+          false
+        else
+          if acc && al_char_count_map[message_char] >= message_char_count, do: true, else: false
+        end
+      end)
+    else
+      false
+    end
   end
 
   defp to_char_count_map(str) do
